5057fc99ba790963dbcb8eb7a98d3ba2c0794661
SakethDodda12 <sakethdodda57@gmail.com>
2025-07-29 17:30:54 -0500

    Add files via upload

---
+++ storing birthdays.py
@@ -0,0 +1,7 @@
+jhon = ("january, 23, 2012")
+george = ("march 24, 2014")
+lucas = ("october 28, 2012")
+
+print ("jhons birth day is", jhon)
+print ("georges birth day is", george)
+print ("lucases birth day is", lucas)
